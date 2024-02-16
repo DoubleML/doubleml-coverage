19c46acb48669d78459f767d037396644c456a86
Sven Klaassen <47529404+SvenKlaassen@users.noreply.github.com>
2024-02-16 12:57:06 +0100

    update irm_ate simulation

--- simulations/irm/irm_ate_coverage.py
+++ simulations/irm/irm_ate_coverage.py
@@ -24,14 +24,21 @@ for i in range(n_rep):
 
 # set up hyperparameters
 hyperparam_dict = {
-    "learner": [("Lasso_Logistic", (LassoCV(), LogisticRegressionCV())),
-                ("RF_RF", (RandomForestRegressor(n_estimators=100, max_features=20, max_depth=5, min_samples_leaf=2),
-                           RandomForestClassifier(n_estimators=100, max_features=20, max_depth=5, min_samples_leaf=2)))],
+    "learner_g": [("Lasso", LassoCV()),
+                  ("Random Forest",
+                   RandomForestRegressor(n_estimators=100, max_features=20, max_depth=5, min_samples_leaf=2))],
+    "learner_m": [("Logistic Regression", LogisticRegressionCV()),
+                  ("Random Forest",
+                   RandomForestClassifier(n_estimators=100, max_features=20, max_depth=5, min_samples_leaf=2))],
     "level": [0.95, 0.90]
 }
 
 # set up the results dataframe
-df_results_detailed = pd.DataFrame(columns=["coverage", "ci_length", "learner", "level", "repetition"])
+df_results_detailed = pd.DataFrame(
+    columns=["Coverage", "CI Length",
+             "Bias",
+             "Learner g", "Learner m",
+             "level", "repetition"])
 
 # start simulation
 np.random.seed(42)
@@ -42,31 +49,40 @@ for i_rep in range(n_rep):
     # define the DoubleML data object
     obj_dml_data = dml.DoubleMLData(datasets[i_rep], 'y', 'd')
 
-    for learner_idx, (learner_name, (ml_g, ml_m)) in enumerate(hyperparam_dict["learner"]):
-        # Set machine learning methods for g & m
-        dml_irm = dml.DoubleMLIRM(
-            obj_dml_data=obj_dml_data,
-            ml_g=ml_g,
-            ml_m=ml_m,
-        )
-        dml_irm.fit(n_jobs_cv=5)
+    for learner_g_idx, (learner_g_name, ml_g) in enumerate(hyperparam_dict["learner_g"]):
+        for learner_m_idx, (learner_m_name, ml_m) in enumerate(hyperparam_dict["learner_m"]):
+            # Set machine learning methods for g & m
+            dml_irm = dml.DoubleMLIRM(
+                obj_dml_data=obj_dml_data,
+                ml_g=ml_g,
+                ml_m=ml_m,
+            )
+            dml_irm.fit(n_jobs_cv=5)
 
-        for level_idx, level in enumerate(hyperparam_dict["level"]):
-            confint = dml_irm.confint(level=level)
-            coverage = (confint.iloc[0, 0] < theta) & (theta < confint.iloc[0, 1])
-            ci_length = confint.iloc[0, 1] - confint.iloc[0, 0]
+            for level_idx, level in enumerate(hyperparam_dict["level"]):
+                confint = dml_irm.confint(level=level)
+                coverage = (confint.iloc[0, 0] < theta) & (theta < confint.iloc[0, 1])
+                ci_length = confint.iloc[0, 1] - confint.iloc[0, 0]
 
-            df_results_detailed = pd.concat(
-                (df_results_detailed,
-                 pd.DataFrame({
-                    "coverage": coverage.astype(int),
-                    "ci_length": confint.iloc[0, 1] - confint.iloc[0, 0],
-                    "learner": learner_name,
-                    "level": level,
-                    "repetition": i_rep}, index=[0])),
-                ignore_index=True)
+                df_results_detailed = pd.concat(
+                    (df_results_detailed,
+                     pd.DataFrame({
+                        "Coverage": coverage.astype(int),
+                        "CI Length": confint.iloc[0, 1] - confint.iloc[0, 0],
+                        "Bias": abs(dml_irm.coef[0] - theta),
+                        "Learner g": learner_g_name,
+                        "Learner m": learner_m_name,
+                        "level": level,
+                        "repetition": i_rep}, index=[0])),
+                    ignore_index=True)
 
-df_results = df_results_detailed.groupby(["learner", "level"]).agg({"coverage": "mean", "ci_length": "mean"}).reset_index()
+df_results = df_results_detailed.groupby(
+    ["Learner g", "Learner m", "level"]).agg(
+        {"Coverage": "mean",
+         "CI Length": "mean",
+         "Bias": "mean",
+         "repetition": "count"}
+    ).reset_index()
 print(df_results)
 
 # save results
