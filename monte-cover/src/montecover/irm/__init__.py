63f3fede3f703760d115cf148970298725fb8fd7
SvenKlaassen <sven.klaassen@uni-hamburg.de>
2025-06-03 14:35:35 +0200

    add irm cate to monte cover

--- monte-cover/src/montecover/irm/__init__.py
+++ monte-cover/src/montecover/irm/__init__.py
@@ -1,9 +1,11 @@
 """Monte Carlo coverage simulations for IRM."""
 
 from montecover.irm.irm_ate import IRMATECoverageSimulation
+from montecover.irm.irm_cate import IRMCATECoverageSimulation
 from montecover.irm.irm_gate import IRMGATECoverageSimulation
 
 __all__ = [
     "IRMATECoverageSimulation",
+    "IRMCATECoverageSimulation",
     "IRMGATECoverageSimulation",
 ]
