43b40b4a35a4787b4e083d2b1a3ce5e514366e55
SvenKlaassen <sven.klaassen@uni-hamburg.de>
2025-06-04 09:45:38 +0200

    add atte sensitivity to monte cover

--- monte-cover/src/montecover/irm/__init__.py
+++ monte-cover/src/montecover/irm/__init__.py
@@ -3,6 +3,7 @@
 from montecover.irm.irm_ate import IRMATECoverageSimulation
 from montecover.irm.irm_ate_sensitivity import IRMATESensitivityCoverageSimulation
 from montecover.irm.irm_atte import IRMATTECoverageSimulation
+from montecover.irm.irm_atte_sensitivity import IRMATTESensitivityCoverageSimulation
 from montecover.irm.irm_cate import IRMCATECoverageSimulation
 from montecover.irm.irm_gate import IRMGATECoverageSimulation
 
@@ -10,6 +11,7 @@ __all__ = [
     "IRMATECoverageSimulation",
     "IRMATESensitivityCoverageSimulation",
     "IRMATTECoverageSimulation",
+    "IRMATTESensitivityCoverageSimulation",
     "IRMCATECoverageSimulation",
     "IRMGATECoverageSimulation",
 ]
