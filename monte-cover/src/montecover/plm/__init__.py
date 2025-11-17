445c30f030d66ad123c30d907a0ddfc49e0e82fc
SvenKlaassen <sven.klaassen@uni-hamburg.de>
2025-11-17 17:06:21 +0100

    add plr tuning example

--- monte-cover/src/montecover/plm/__init__.py
+++ monte-cover/src/montecover/plm/__init__.py
@@ -2,6 +2,7 @@
 
 from montecover.plm.pliv_late import PLIVLATECoverageSimulation
 from montecover.plm.plr_ate import PLRATECoverageSimulation
+from montecover.plm.plr_ate_tune import PLRATETuningCoverageSimulation
 from montecover.plm.plr_ate_sensitivity import PLRATESensitivityCoverageSimulation
 from montecover.plm.plr_cate import PLRCATECoverageSimulation
 from montecover.plm.plr_gate import PLRGATECoverageSimulation
@@ -12,4 +13,5 @@ __all__ = [
     "PLRGATECoverageSimulation",
     "PLRCATECoverageSimulation",
     "PLRATESensitivityCoverageSimulation",
+    "PLRATETuningCoverageSimulation",
 ]
